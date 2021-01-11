e410e3f5a4d156159f1b3a95b58fdc9eecfacb09
Kumar Sandeep Ramanath <kumarsandeepr89@gmail.com>
2021-01-11 22:09:07 +0530

    Changes in coding challenge 4

--- 01-Fundamentals-Part-1/starter/codingChallenge4.js
+++ 01-Fundamentals-Part-1/starter/codingChallenge4.js
@@ -1,6 +1,10 @@
-const bill = 275
+const bill = 275;
 // const bill = 40
 // const bill = 430
 
-const tip = (bill >= 50 && bill <= 300) ? (0.15 * bill) : (0.20 * bill)
-console.log(`The bill is ${bill} and the tip is ${tip} and hence the total bill is ${bill + tip}`)
+const tip = bill >= 50 && bill <= 300 ? 0.15 * bill : 0.2 * bill;
+console.log(
+  `The bill is ${bill} and the actual tip is ${tip} and hence the total bill is ${
+    bill + tip
+  }`
+);
