8b7dfc3f9cbfc3e75a4ee29461b2b17ea8163140
Kumar Sandeep Ramanath <kumarsandeepr89@gmail.com>
2021-01-13 23:24:22 +0530

    Gamify

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,17 @@
 'use strict';
+/*console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+document.querySelector('.guess').value = 25;*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (guess) {
+    console.log('NUmber guessed');
+  } else {
+    document.querySelector('.message').textContent = 'No Number';
+  }
+});
