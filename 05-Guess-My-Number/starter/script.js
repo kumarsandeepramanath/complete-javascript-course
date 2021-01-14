12e5c0a3f3cc6d6aa7846d0bff3f03640c831837
Kumar Sandeep Ramanath <kumarsandeepr89@gmail.com>
2021-01-14 22:18:39 +0530

    Guess game changes

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -6,12 +6,36 @@ document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 25;*/
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
-  if (guess) {
-    console.log('NUmber guessed');
-  } else {
+  if (!guess) {
     document.querySelector('.message').textContent = 'No Number';
+  } else if (guess === secretNumber) {
+    document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector('.message').textContent = 'Correct Number';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Number is too high';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Number is too low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
+
+document.querySelector('btn again').addEventListener;
