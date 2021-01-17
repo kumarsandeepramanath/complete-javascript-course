80045135de776d0beae48e0116f5cacffe6d0ac4
Kumar Sandeep Ramanath <kumarsandeepr89@gmail.com>
2021-01-17 22:24:45 +0530

    Modal and Pig game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+const rollDice = document.querySelector('.btn--roll');
+const diceValue = document.querySelector('.dice');
+
+document.querySelector('#score--0').textContent = 0;
+document.querySelector('#score--1').textContent = 0;
+document.querySelector('.dice').classList.add('hidden');
+const generateRandomDiceNumber = function () {
+  return Math.round(Math.random() * 6);
+};
+//User rolls a dice
+rollDice.addEventListener('click', function () {
+  console.log(`Random Number generated is ${generateRandomDiceNumber()}`);
+  document.querySelector('.body').sty;
+});
