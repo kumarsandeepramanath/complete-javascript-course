84819b52c3081cee0e7a2b72aebd32fc8cd442d8
Kumar Sandeep Ramanath <kumarsandeepr89@gmail.com>
2021-01-24 23:47:32 +0530

    DS

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+function oneWord(str) {
+  return str.replace(/ /g, '');
+}
+
+function firstWordInCaps(sentence) {
+  const [firstWord, ...others] = sentence.split(' ');
+  return [firstWord.toUpperCase(), ...others].join(' ');
+}
+
+// console.log(firstWordInCaps('Javscript is great'));
+
+function transform(str, fn) {
+  console.log(`Before transformation : ${str}`);
+  console.log(`The ${str} has been transformed to ${fn(str)}`);
+  console.log(`After transformation ${str}`);
+}
+
+// transform('Javascript is great', firstWordInCaps);
+transform('Javascript is great', oneWord);
